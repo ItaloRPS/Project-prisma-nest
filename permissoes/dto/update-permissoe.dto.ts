56a8add37dfd921095045d9c79f7bc48b8dc3dac
ItaloRPS <99054819+ItaloRPS@users.noreply.github.com>
2024-05-14 00:39:02 -0300

    Delete permissoes directory

--- permissoes/dto/update-permissoe.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePermissoeDto } from './create-permissoe.dto';
-
-export class UpdatePermissoeDto extends PartialType(CreatePermissoeDto) {}
