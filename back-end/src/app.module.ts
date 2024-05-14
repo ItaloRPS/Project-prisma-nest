140970aadd705c6f2860b20ea27e003af9487f35
ItaloRPS <99054819+ItaloRPS@users.noreply.github.com>
2024-05-14 00:40:21 -0300

    Add files via upload

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,13 +1,84 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+/* eslint-disable prettier/prettier */
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { PostModule } from './post/post.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { EmpresaModule } from './empresa/empresa.module';
+import { ConfigModule } from '@nestjs/config';
+import { LeadModule } from './lead/lead.module';
+import { PerfilModule } from './perfil/perfil.module';
+import { PermissoesModule } from './permissoes/permissoes.module';
+import { ProdutoModule } from './produto/produto.module';
+import { LoteModule } from './lote/lote.module';
+import { AcessoModule } from './acesso/acesso.module';
+import { ConfigprodutoModule } from './configproduto/configproduto.module';
+import { ModeloModule } from './modelo/modelo.module';
+import { TemaModule } from './tema/tema.module';
+import { TemaItemModule } from './tema-item/tema-item.module';
+import { CidadeModule } from './cidade/cidade.module';
+import { EstadoModule } from './estado/estado.module';
+import { PaisModule } from './pais/pais.module';
+import { FileModule } from './file/file.module';
+import { MessageModule } from './message/message.module';
+import { WhatsappModule } from './whatsapp/whatsapp.module';
+import { EmailService } from './common/email/email.service';
+ import { LojistaModule } from './lojista/lojista.module';
+ import { ScheduleModule } from '@nestjs/schedule';
+import { AuthorizationMiddleware } from './auth/middlewares/AuthorizationMiddleware';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, PostModule],
+  imports: [
+    ConfigModule.forRoot(),
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    EmpresaModule,
+    LeadModule,
+    PerfilModule,
+    PermissoesModule,
+    ProdutoModule,
+    LoteModule,
+    AcessoModule,
+    ConfigprodutoModule,
+    ModeloModule,
+    TemaModule,
+    TemaItemModule,
+    CidadeModule,
+    EstadoModule,
+    PaisModule,
+    FileModule,
+    MessageModule,
+    WhatsappModule,
+    LojistaModule,
+    ScheduleModule.forRoot(),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService, ],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    // {
+    //   provide: APP_FILTER,
+    //   useClass: AllExceptionsFilter,
+    // },
+    EmailService,
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+  consumer.apply(AuthorizationMiddleware).exclude({ path: '/api/v1/login', method: RequestMethod.POST }).forRoutes('*');
+  }
+
+}
+
--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,32 +1,42 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConflictInterceptor } from './common/erros/interceptor/conflictError.intercepto';
-import { NotFoundInterceptor } from './common/erros/interceptor/notFoundError.interceptor';
-import { DatabaseInterceptor } from './common/erros/interceptor/databaseError.interceptor';
-import { UnautorizedInterceptor } from './common/erros/interceptor/unautorizedError.interceptor';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConflictInterceptor } from './common/errors/interceptors/conflict.interceptor';
+import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
+import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
+import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interceptor';
 
-
-export async function bootstrap() {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.enableShutdownHooks();
+
   const config = new DocumentBuilder()
-  .setTitle('Simple API')
-  .setDescription('The simple API')
-  .setVersion('1.0')
-  .build();
+    .setTitle('Documentação com Swagger - Multleads')
+    .setDescription(
+      'O Swagger (aka OpenApi) é uma biblioteca muito conhecida no universo backend, estando disponível para diversas linguagens e frameworks. Ela gera um site interno no seu backend que descreve, com muitos detalhes, cada endpoint e estrutura de entidades presentes na sua aplicação.',
+    )
+    .setVersion('1.0')
+    .build();
+
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist:true,
-    forbidNonWhitelisted:true,
-    transform:true
-  }))
-  //app.useGlobalFilters(new HttpExceptionFilter())
-  app.useGlobalInterceptors(new ConflictInterceptor)
-  app.useGlobalInterceptors(new DatabaseInterceptor)
-  app.useGlobalInterceptors(new UnautorizedInterceptor)
-  app.useGlobalInterceptors(new NotFoundInterceptor)
-  await app.listen(process.env.PORT ||3000);
+  SwaggerModule.setup('api/', app, document);
+
+  // Pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.useGlobalInterceptors(new ConflictInterceptor());
+  app.useGlobalInterceptors(new DatabaseInterceptor());
+  app.useGlobalInterceptors(new UnauthorizedInterceptor());
+  app.useGlobalInterceptors(new NotFoundInterceptor());
+  await app.listen(process.env.PORT || 3100);
 }
+
 bootstrap();
