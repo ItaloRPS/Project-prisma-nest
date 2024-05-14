beaffd1592b43c594e8ff2920e609ea87d768fd8
ItaloRPS <99054819+ItaloRPS@users.noreply.github.com>
2024-05-14 00:38:34 -0300

    Delete tema-item directory

--- tema-item/dto/update-tema-item.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateTemaItemDto } from './create-tema-item.dto';
-
-export class UpdateTemaItemDto extends PartialType(CreateTemaItemDto) {}
--- tema-item/entities/tema-item.entity.ts
+++
@@ -1,9 +0,0 @@
-import { temaitem } from '@prisma/client';
-
-export class TemaItemEntity implements temaitem {
-  idtemaitem: number;
-  idtema: number;
-  descricao: string;
-  link: string;
-  icone: string;
-}
